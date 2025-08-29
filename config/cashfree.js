b9c0325c4a4ae66858629db84fc0cbc32a572071
Shubham Kumar <shubhamdtg898@gmail.com>
2025-08-29 10:16:11 +0530

    Commited

--- config/cashfree.js
+++
@@ -1,6 +0,0 @@
-export default {
-  baseUrl: "https://api.cashfree.com/pg/orders",  // ✅ Production URL only
-  appId: process.env.CASHFREE_APP_ID,             // From Cashfree dashboard (Production keys)
-  secretKey: process.env.CASHFREE_SECRET_KEY,     // From Cashfree dashboard (Production keys)
-  apiVersion: "2022-09-01",
-};
